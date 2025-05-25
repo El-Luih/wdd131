9f7a1e8f21d6a74b3d15731d518d1eceabdcd206
El-Luih <luisalejandro632@gmail.com>
2025-05-25 04:24:14 -0600

    Place Page

---
+++ scripts/place.js
@@ -0,0 +1,19 @@
+const temperatureCell = document.querySelector("#temp-cell");
+const windSpeedCell = document.querySelector("#wind-cell");
+const windChillCell = document.querySelector("#chill-cell");
+
+let temperature = 9;
+let windSpeed = 14;
+
+temperatureCell.innerHTML = `${temperature} °C`;
+windSpeedCell.innerHTML = `${windSpeed} km/h`;
+
+if (temperature <= 10 && windSpeed > 4.8) {
+    let windChill = calculatWindChill(temperature, windSpeed);
+    windChillCell.innerHTML = `${windChill.toFixed(2    )}°C`
+}
+
+function calculatWindChill(temperature, windSpeed) {
+    const windChill = 13.12 + 0.6215 * temperature - 11.37 * windSpeed ** 0.16 + 0.3965 * temperature * windSpeed ** 0.16;
+    return windChill;
+}
